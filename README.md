5e56550b8a7ff745b370b6be7ae0ac15ac9490d9
Aluis Marte <aluismarte@gmail.com>
2026-06-01 22:05:36 -0400

    Starting project

---
+++ README.md
@@ -0,0 +1,11 @@
+# JAI-DOc
+
+![Java](https://img.shields.io/badge/Java-25-red.svg)
+![Spring-Boot](https://img.shields.io/badge/Spring%20boot-4.0.5-green.svg)
+![Maven](https://img.shields.io/badge/maven-3.9.15-blue.svg)
+
+# How to compile
+
+```bash
+mvn clean package
+```
---
+++ src/main/java/com/purrbyte/ai/JAIDoc.java
@@ -0,0 +1,39 @@
+package com.purrbyte.ai;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+
+import java.util.UUID;
+
+@Slf4j
+@SpringBootApplication
+public class JAIDoc {
+
+    static void main(String[] args) {
+        String appId = UUID.randomUUID().toString();
+        System.setProperty("APP_ID", appId);
+        log.info("APP_ID: {}", appId);
+        SpringApplication springApplication = new SpringApplication(JAIDoc.class);
+        springApplication.setApplicationStartup(prepareStartup());
+        springApplication.run(args);
+    }
+
+    private static BufferingApplicationStartup prepareStartup() {
+        int startupSize = getStartupSize();
+        String startupFilter = getStartupFilter();
+        log.info("Preparing startup Buffer: {} - {}", startupSize, startupFilter);
+        BufferingApplicationStartup bufferingApplicationStartup = new BufferingApplicationStartup(startupSize);
+        bufferingApplicationStartup.addFilter(startupStep -> startupStep.getName().matches(startupFilter));
+        return bufferingApplicationStartup;
+    }
+
+    private static int getStartupSize() {
+        return 2048;
+    }
+
+    private static String getStartupFilter() {
+        return "spring.beans.instantiate";
+    }
+}
