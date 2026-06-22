56086421e92683cca4fd8ab0fc3f49aaa2fcbcb9
Aluis Marte <aluismarte@gmail.com>
2026-06-21 23:15:03 -0400

    refactor(config): configure RestClient to follow HTTP redirects
    Override prepareConnection to enable instance-level redirect following. This ensures the REST client does not stop at 3xx responses but automatically follows them.

--- src/main/java/com/purrbyte/ai/configuration/RestConfiguration.java
+++ src/main/java/com/purrbyte/ai/configuration/RestConfiguration.java
@@ -1,4 +1,27 @@
 package com.purrbyte.ai.configuration;
 
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+@Configuration
 public class RestConfiguration {
+
+    @Bean
+    public RestClient restClient(RestClient.Builder builder) {
+        return builder
+                .requestFactory(new SimpleClientHttpRequestFactory() {
+                    @Override
+                    protected void prepareConnection(@NonNull HttpURLConnection connection, @NonNull String httpMethod) throws IOException {
+                        super.prepareConnection(connection, httpMethod);
+                        connection.setInstanceFollowRedirects(true);
+                    }
+                })
+                .build();
+    }
 }
